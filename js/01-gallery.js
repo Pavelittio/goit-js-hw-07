1e9e4766d377d7daeaae2ff7ac802067cdd10673
Pavel Yatsenko <106490150+Pavelittio@users.noreply.github.com>
2022-10-25 21:50:49 +0300

    hw-js-07_1
    Требует доработки. Модалка не открывается, а открывается внизу

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeImgGalaryRender = ({preview, original, description}) => {
+return `
+<div class="gallery__item">
+<a class="gallery__link" href"${original}">
+<img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`
+};
+const imgGalary = galleryItems.map(makeImgGalaryRender).join(``);
+const divGalaryRef = document.querySelector(`.gallery`);
+divGalaryRef.innerHTML = imgGalary;
+
+
+divGalaryRef.addEventListener(`click`, onTagContainerClick);
+function onTagContainerClick (event){
+  event.preventDefault();
+  if (event.target.nodeName !== `IMG`)
+  return;
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`)
+instance.show()
+};
+
+divGalaryRef.addEventListener("keydown", (event)=>{
+  if (event.code === "Escape"){
+    instance.close();
+  }
+})
+
